04eab43c6e0e65f9087ef71d968d84c4e677d4e1
EduHSMattos <eduhmattos@hotmail.com>
2023-09-15 22:07:24 -0300

    15/09

--- pages/contador.js
+++ pages/contador.js
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
 import Breadcrumbs from "@/app/components/Breadcrumbs";
 import Button from "@/app/components/Button";
+import Navbar from "@/app/components/Navbar";
 
+import React, {useState} from "react";
 
 const ContadorApp = () => {
-    
+
     let [contador, setContador] = useState(0);
 
     const incrementar = () => {
@@ -17,21 +18,30 @@ const ContadorApp = () => {
 
     return(
         <div>
+            <Navbar></Navbar>
             <Breadcrumbs></Breadcrumbs>
             <h1>Contador usando useState</h1>
 
             <p>Contagem: {contador} </p>
 
-            <button onClick={incrementar}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Incrementar</button>
+            <button 
+                onClick={incrementar}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Incrementar
+            </button>
 
-            <button onClick={decrementar}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Decrementar</button>
+            <button 
+                onClick={decrementar}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Decrementar
+            </button>
 
-
+            {/*<Button onClick={incrementar}>Incrementar</Button>*/}
 
         </div>
-    )    
+    )
 }
 
 export default ContadorApp;
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,13 +1,15 @@
-import React from "react";
 import Breadcrumbs from "@/app/components/Breadcrumbs";
+import Navbar from "@/app/components/Navbar";
+import React from "react";
 
 const About = () => {
     return(
         <div>
+            <Navbar></Navbar>
             <Breadcrumbs></Breadcrumbs>
-            <h1>Aqui é a página sobre.</h1>
+            <h1>Aqui é a pagina sobre! Seja muito bem vindo!</h1>
         </div>
-    )    
+    )
 }
 
 export default About;
